b5f549b2d5a765f63100fa6537d813c8ecddbdeb
ProgKea <layerk0@protonmail.com>
2025-10-26 12:45:25 +0100

    add raylib dependency for convenience

---
+++ build_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -xe
+
+mkdir -p build
+
+CFLAGS="-Wall -Wextra -ggdb -fsanitize=address -I./Dependencies/raylib-5.5_linux_amd64/include"
+LFLAGS="-L./Dependencies/raylib-5.5_linux_amd64/lib -l:libraylib.a"
+
+clang $CFLAGS -o build/pid Source/PID/pid_main.c $LFLAGS
